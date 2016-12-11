078cb61333e1c9f6c9b5b7523a4a32455cac46a7
felipeDuitama <skamilo9406@gmail.com>
2016-12-10 23:01:37 -0500

    prueba get base de datos

---
+++ Ferreteria/routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var users = require('../Controllers/users.js');
+
+/* GET users listing. */
+router.route('/') 
+ .get(users.findAll);
+
+
+module.exports = router;
