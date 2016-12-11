d821ed9e4db7bb9296bab0651c23479670ab0134
Camilo <skamilo9406@gmail.com>
2016-12-11 11:32:19 -0500

    actualizando repo

--- Ferreteria/routes/users.js
+++ Ferreteria/routes/users.js
@@ -3,15 +3,8 @@ var router = express.Router();
 var users = require('../Controllers/users.js');
 
 /* GET users listing. */
-<<<<<<< HEAD
 router.route('/')
  .get(users.findAll);
-=======
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: users.findAll });
-// });
-router.get('/', users.findAll);
->>>>>>> 27a01c5a994f97738c2aee97e385a19a4881b706
 
 
 module.exports = router;
