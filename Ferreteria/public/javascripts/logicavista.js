ffc10a90ff3735d10a42ec2189c1c60b78d55441
Camilo <skamilo9406@gmail.com>
2016-12-12 01:38:29 -0500

    vistas

---
+++ Ferreteria/public/javascripts/logicavista.js
@@ -0,0 +1,146 @@
+$(document).ready(function() {
+    getProductos()
+    getOrdenCompra()
+    getUsuario()
+    infoProveedor = getProveedores();
+    var item = 0;
+    $(".claseProducto").click(function() {
+        item += 1;
+        var aux;
+        aux = aux + "<tr id=tabla" + item + ">";
+        aux = aux + "<td id=item" + item + ">" + item + "</td>";
+        aux = aux + "<td id=producto" + item + ">" + $(".OS option:selected").text() + "</td>";
+        aux = aux + "<td id=precio" + item + ">" + "precio</td>";
+        aux = aux + "<td>" + "<input type='text' id=cantidad" + item + ">" + "</td>";
+        aux = aux + "</tr>";
+        $("#detalle").append(aux);
+    });
+    $(".claseProveedor").click(function() {
+        for (var i = 0; i < infoProveedor.length; i++) {
+            if (infoProveedor[i].NOMBREPERSONA == $(".OS option:selected").text()) {
+                $("#cedulaProveedor").text(infoProveedor[i].NUMCEDULA);
+                $("#nombreProveedor").text(infoProveedor[i].NOMBREPERSONA);
+                $("#direccionProveedor").text(infoProveedor[i].DIRECCIONPERSONA);
+                $("#telefonoProveedor").text(infoProveedor[i].TELEONOPERSONA);
+            }
+        }
+    });
+    $("#enviar").click(function() {
+        var auxNombre = "";
+        var auxCantidad = "";
+        var aux = '{"CANTIDAD":' + '"' + item + '"' + ',';
+        for (var i = 1; i <= item; i++) {
+            aux = aux + '"PRODUCTO' + i + '":'
+            aux = aux + '{"NOMBRE":' + '"' + $("#producto" + i).text() + '"' + ", ";
+            if (isNaN($("#cantidad" + i).val()) || $("#cantidad" + i).val() == "") {
+                alert("Cantidad invalida en item " + i)
+                return
+            }
+            aux = aux + ' "CANTIDAD": "' + $("#cantidad" + i).val() + '"' + "},";
+        }
+        aux = aux + '"NUMEROTRANSACCION":' + '"' + $("#ordenCompra").text() + '"' + ",";
+        aux = aux + '"IDENTIFICACION": "' + $("#idE").text() + '"' + ",";
+        aux = aux + '"NUMCEDULA": "' + $("#cedulaProveedor").text() + '"' + ",";
+        aux = aux + '"FECHATRANSACCION": "' + $("#fecha").text() + '"' + "}";
+        var obj = JSON.parse(aux);
+        $.ajax({
+            url: 'http://localhost:3000/ordencompra',
+            type: 'POST',
+            data : obj,
+            dataType: 'json',
+            success: function(json) {
+                for (var i = 0; i < json.length; i++) {
+                    $("#OS").append("<option class='claseProducto'>"
+                        json[i].NOMBRE "</option>");
+                }
+
+            },
+            error: function(xhr, status) {
+                alert('Disculpe, existió un problema');
+            },
+            complete: function(xhr, status) {
+                alert('Petición realizada');
+            }
+        });
+
+    });
+});
+
+function getUsuario() {
+    $.ajax({
+        url: 'http://localhost:3000/usuario',
+        type: 'GET',
+        dataType: 'json',
+        success: function(json) {
+            $("#idE").text(json.IDENTIFICACION);
+            $("#nombreE").text(json.NOMBREEMPLEADO);
+        },
+        error: function(xhr, status) {
+            alert('Disculpe, existió un problema');
+        },
+        complete: function(xhr, status) {
+            alert('Petición realizada');
+        }
+    });
+}
+
+function getOrdenCompra() {
+    $.ajax({
+        url: 'http://localhost:3000/orden',
+        type: 'GET',
+        dataType: 'json',
+        success: function(json) {
+            $("#ordenCompra").text(json.NUMEROTRANSACCION);
+        },
+        error: function(xhr, status) {
+            alert('Disculpe, existió un problema');
+        },
+        complete: function(xhr, status) {
+            alert('Petición realizada');
+        }
+    });
+}
+
+function getProductos() {
+    $.ajax({
+        url: 'http://localhost:3000/productos',
+        type: 'GET',
+        dataType: 'json',
+        success: function(json) {
+            for (var i = 0; i < json.length; i++) {
+                $("#OS").append("<option class='claseProducto'>"
+                    json[i].NOMBRE "</option>");
+            }
+
+        },
+        error: function(xhr, status) {
+            alert('Disculpe, existió un problema');
+        },
+        complete: function(xhr, status) {
+            alert('Petición realizada');
+        }
+    });
+}
+
+function getProveedores() {
+    var infoProveedor;
+    $.ajax({
+        url: 'http://localhost:3000/proveedor',
+        type: 'GET',
+        dataType: 'json',
+        success: function(json) {
+            for (var i = 0; i < json.length; i++) {
+                $("#nombreProveedor").append("<option class='claseProveedor'>"
+                    json[i].NOMBREPERSONA "</option>");
+            }
+            infoProveedor = json;
+        },
+        error: function(xhr, status) {
+            alert('Disculpe, existió un problema');
+        },
+        complete: function(xhr, status) {
+            alert('Petición realizada');
+        }
+    });
+    return infoProveedor;
+}
